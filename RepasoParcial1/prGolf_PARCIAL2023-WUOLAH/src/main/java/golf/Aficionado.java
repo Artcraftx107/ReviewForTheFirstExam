86f967dbcc7c0b6a747e8d9d063bf48a67e800ce
Artcraft <162491480+Artcraftx107@users.noreply.github.com>
2024-04-08 14:06:08 +0200

    Examen Parcial 2023 prGolf (Unfinished)

---
+++ RepasoParcial1/prGolf_PARCIAL2023-WUOLAH/src/main/java/golf/Aficionado.java
@@ -0,0 +1,16 @@
+package golf;
+
+public class Aficionado {
+    private int skillIssue;
+    public Aficionado(String name, int score){
+        if(1<=score && score<=54){
+            Jugador rookie = new Jugador(name);
+            this.skillIssue = score;
+        }else{
+            throw new IllegalArgumentException("El handicap indicado no es valido.");
+        }
+    }
+    public int score(){
+        int finalScore = 
+    }
+}
